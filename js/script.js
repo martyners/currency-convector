887fb419c4b4e49fb3d1c7a2d763843d34af7f97
Martyna Michalak-Halinska <martyna.michalak@gmail.com>
2020-07-07 13:02:23 +0200

    Added arrow function update

--- js/script.js
+++ js/script.js
@@ -1,29 +1,42 @@
-let polishZlotyForEuro = document.querySelector(".js-polishZlotyForEuro");
-let euroRate = document.querySelector(".js-euroRate");
-let plEuroResponse = document.querySelector(".js-plEuroResponse")
-let formElementPlEuro = document.querySelector(".js-formPlEuro");
+{
 
-formElementPlEuro.addEventListener("input", (event) => {
-    let exchange = (polishZlotyForEuro.value * euroRate.value);
-    plEuroResponse.innerText = `${polishZlotyForEuro.value} zł możesz wymienić na ${exchange.toFixed(2)} euro`;
-})
+    const onFormInput = () => {
+        const polishZlotyForEuro = document.querySelector(".js-polishZlotyForEuro");
+        const euroRate = document.querySelector(".js-euroRate");
+        const plEuroResponse = document.querySelector(".js-plEuroResponse")
+        const exchange = (polishZlotyForEuro.value * euroRate.value);
+        
+        plEuroResponse.innerText = `${polishZlotyForEuro.value} zł możesz wymienić na ${exchange.toFixed(2)} euro`;
+    };
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const polishZlotyForOther = document.querySelector(".js-polishZlotyForOther");
+        const plOtherResponse = document.querySelector(".js-plOtherResponse");
+        const currencySelected = document.querySelector(".js-currencySelected");
+
+        if (currencySelected.value === "USD") {
+            const exchange = (polishZlotyForOther.value * 0.2526)
+            plOtherResponse.innerText = `${polishZlotyForOther.value} zł możesz wymienić na ${exchange.toFixed(2)} USD`
+        } else if (currencySelected.value === "GBP") {
+            const exchange = (polishZlotyForOther.value * 0.2039)
+            plOtherResponse.innerText = `${polishZlotyForOther.value} zł możesz wymienić na ${exchange.toFixed(2)} GBP`
+        } else {
+            const exchange = (polishZlotyForOther.value * 0.3519)
+            plOtherResponse.innerText = `${polishZlotyForOther.value} zł możesz wymienić na ${exchange.toFixed(2)} SGD`
+        };
+    };
 
 
-let polishZlotyForOther = document.querySelector(".js-polishZlotyForOther");
-let formElementPlOther = document.querySelector(".js-formPlOther");
-let plOtherResponse = document.querySelector(".js-plOtherResponse");
-let currencySelected = document.querySelector(".js-currencySelected");
+    const init = () => {
+        const formElementPlEuro = document.querySelector(".js-formPlEuro");
+        formElementPlEuro.addEventListener("input", (onFormInput));
 
-formElementPlOther.addEventListener("submit", (event) => {
-    event.preventDefault();
-    if (currencySelected.value === "USD") {
-        let exchange = (polishZlotyForOther.value * 0.2526)
-        plOtherResponse.innerText = `${polishZlotyForOther.value} zł możesz wymienić na ${exchange.toFixed(2)} USD`
-    } else if (currencySelected.value === "GBP") {
-        let exchange = (polishZlotyForOther.value * 0.2039)
-        plOtherResponse.innerText = `${polishZlotyForOther.value} zł możesz wymienić na ${exchange.toFixed(2)} GBP`
-    } else {
-        let exchange = (polishZlotyForOther.value * 0.3519)
-        plOtherResponse.innerText = `${polishZlotyForOther.value} zł możesz wymienić na ${exchange.toFixed(2)} SGD`
-    }
-});
+        const formElementPlOther = document.querySelector(".js-formPlOther");
+        formElementPlOther.addEventListener("submit", (onFormSubmit));
+    };
+
+    init();
+
+};
